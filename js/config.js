cfc49a6d818e27db1572b8b4223019fa6a7fdae9
dananasr2004 <dananasr871@gmail.com>
2024-10-26 15:36:13 +0300

    Create: player and add moves

--- js/config.js
+++ js/config.js
@@ -1,15 +1,5 @@
-// var config = {
-//   type: Phaser.AUTO,
-//   width: 800,
-//   height: 600,
-//   scene: {
-//     preload: preload,
-//     create: create,
-//     // update: update,
-//   },
-// };
+let maze, player, cursors;
 
-// var game = new Phaser.Game(config);
 var config = {
   type: Phaser.AUTO,
   width: 800,
@@ -17,31 +7,33 @@ var config = {
   scene: {
     preload: function () {
       this.load.image("sky", "assets/sky.png");
-      this.load.image("ground", "assets/platform.png");
-      this.load.image("vertical", "assets/vertical.png");
+      this.load.spritesheet("dude", "assets/dude.png", {
+        frameWidth: 32,
+        frameHeight: 48,
+      });
+      this.load.image("tile", "assets/tile.jpg");
     },
     create: function () {
       this.add.image(400, 300, "sky");
+      platforms = this.physics.add.staticGroup();
+      maze = [
+        [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
+        [0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0],
+        [0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1],
+        [0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1],
+        [0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1],
+        [0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1],
+        [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [0, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1],
+        [0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1],
+        [0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 1],
+        [0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1],
+        [0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      ];
 
-   
-        maze = [
-            [0,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1,1,1],
-            [0,1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0,0,0,1],
-            [0,1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 1,0,0],
-            [0,1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1,0,1,1],
-            [0,1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0,0,0,1],
-            [0,1,1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1,0,0, 1],
-            [0,1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0,1,1,0 ,1],
-            [0,1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0,0,0, 1],
-            [0,1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0,0,1,0, 1],
-            [0,1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0,1,1,1, 1],
-            [0,1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1,1,0,0, 1],
-            [0,1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0,1, 0,0, 1],
-            [0,1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1,0,0,0, 1],
-            [0,0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0,0,0, 1],
-            [0,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1,1,1,]
-        ]
-        
       const graphics = this.add.graphics(); //graphic obj
 
       const tileW = 40; //tile dimensions
@@ -50,20 +42,62 @@ var config = {
       for (let row = 0; row < maze.length; row++) {
         for (let col = 0; col < maze[row].length; col++) {
           if (maze[row][col] === 1) {
-            graphics.fillStyle(0x555555, 1);
-            graphics.fillRect(col * tileW, row * tileH, tileW, tileH);
+            const wall = platforms
+              .create(col * tileW, row * tileH, "tile")
+              .setOrigin(0, 0)
+              .refreshBody();
+            wall.setDisplaySize(tileW, tileH);
           }
         }
       }
 
+      this.anims.create({
+        key: "left",
+        frames: this.anims.generateFrameNumbers("dude", { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1,
+      });
 
+      this.anims.create({
+        key: "turn",
+        frames: [{ key: "dude", frame: 4 }],
+        frameRate: 20,
+      });
 
+      this.anims.create({
+        key: "right",
+        frames: this.anims.generateFrameNumbers("dude", { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1,
+      });
 
-      
+      player = this.physics.add.sprite(100, 500, "dude");
+      player.setBounce(0);
+      player.setCollideWorldBounds(true);
+
+      cursors = this.input.keyboard.createCursorKeys();
+
+      this.physics.add.collider(player, platforms);
     },
 
     update: function () {
-      // Future update logic can go here
+      if (cursors.left.isDown) {
+        player.setVelocityX(-160);
+
+        player.anims.play("left", true);
+      } else if (cursors.right.isDown) {
+        player.setVelocityX(160);
+
+        player.anims.play("right", true);
+      } else {
+        player.setVelocityX(0);
+
+        player.anims.play("turn");
+      }
+
+      if (cursors.up.isDown && player.body.touching.down) {
+        player.setVelocityY(-330);
+      }
     },
   },
   physics: {
