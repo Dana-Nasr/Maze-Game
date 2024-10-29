048b2e89818376dce17c063df6f76d1ee3a31bbd
dananasr2004 <dananasr871@gmail.com>
2024-10-30 01:34:21 +0200

    Link: website to game

--- website/script.js
+++ website/script.js
@@ -6,3 +6,12 @@ burger.addEventListener('click', () => {
     navLinks.classList.toggle('active');
     burger.classList.toggle('active');
 });
+
+var startButton = document.getElementById("start");
+
+
+startButton.addEventListener("click", logger);
+
+function logger() {
+  window.location.href = `../user.html`;
+}
