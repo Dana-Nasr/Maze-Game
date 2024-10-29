f6f96069ae2029044b2b3842f8bb6ae3893b652c
dananasr2004 <dananasr871@gmail.com>
2024-10-30 01:38:18 +0200

    Revise: Add link on try now

--- website/script.js
+++ website/script.js
@@ -1,16 +1,15 @@
-const burger = document.querySelector('.burger');
-const navLinks = document.querySelector('.nav-links');
+const burger = document.querySelector(".burger");
+const navLinks = document.querySelector(".nav-links");
+var startButton = document.getElementById("start");
+var start = document.getElementById("start-now");
 
-
-burger.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-    burger.classList.toggle('active');
+burger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+  burger.classList.toggle("active");
 });
 
-var startButton = document.getElementById("start");
-
-
 startButton.addEventListener("click", logger);
+start.addEventListener("click", logger);
 
 function logger() {
   window.location.href = `../user.html`;
