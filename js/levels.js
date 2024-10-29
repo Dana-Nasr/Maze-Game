81045e0c6009cd687548703a5dfb984a17b17594
dananasr2004 <dananasr871@gmail.com>
2024-10-29 21:49:25 +0200

    Add: new page for maze

--- js/levels.js
+++ js/levels.js
@@ -1,27 +1,28 @@
 var index = 0;
 var game;
-
+const h2 = document.querySelector("h2");
 const button1 = document.getElementById("btn1");
 button1.addEventListener("click", function () {
-  button1.disabled = true;
+  stop();
   startGame(1);
 });
 
 const button2 = document.getElementById("btn2");
 button2.addEventListener("click", function () {
-  button2.disabled = true;
+  stop();
   startGame(2);
 });
 
 const button3 = document.getElementById("btn3");
 button3.addEventListener("click", function () {
-  button3.disabled = true;
+  stop();
+
   startGame(3);
 });
 
 function startGame(level) {
   index = level;
-  color=character;
+  color = character;
   clearContentFromDiv();
   game = new Phaser.Game(config);
 }
@@ -31,8 +32,17 @@ function clearContentFromDiv() {
     content.innerHTML = " ";
   }
 }
-if (flag==false){
-  button1.disabled=true;
-  button2.disabled=true;
-  button3.disabled=true;
+if (flag == false) {
+  button1.disabled = true;
+  button2.disabled = true;
+  button3.disabled = true;
+}
+function stop() {
+  h2.style.display = "none";
+  button1.style.display = "none";
+  button2.style.display = "none";
+  button3.style.display = "none";
+  playerBlack.style.display = "none";
+  playerRed.style.display = "none";
+  playerpurple.style.display = "none";
 }
